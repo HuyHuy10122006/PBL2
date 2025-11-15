9ac2c52876a28b8a7db983aa0742f4d736f89b39
hoanghung12122006 <hoanghung12122006@gmail.com>
2025-11-15 09:38:36 +0700

    Update song.h

--- MusicManager/logic/song.h
+++ MusicManager/logic/song.h
@@ -11,18 +11,22 @@ private:
 public:
     Song();
     Song(const QString &title,const QString &artist,int duration,const QString &filePath);
+
     QString getTitle() const;
     QString getArtist() const;
     int getDuration() const;
     QString getFilePath() const;
+
     void setTitle(const QString &t);
     void setArtist(const QString &a);
     void setDuration(int d);
     void setFilePath(const QString &path);
+
     QString toString() const;
     friend QDebug operator<<(QDebug dbg, const Song &song){
         dbg.noquote() << song.toString();
         return dbg;
     }
 };
+
 #endif
